40d0933b36ad89dd02b455fdea0d148a1e9a5b9b
Chris Vilches <felovilches@gmail.com>
2022-09-18 00:26:48 +0900

    fix ports, http, etc

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const API_HOST = process.env.NODE_ENV === "production" ? (
-  "http://cloud.chrisvilches.com/poller"
+  "https://cloud.chrisvilches.com/poller"
 ) : (
   "http://localhost"
 );
 
-export const API_PORT = process.env.NODE_ENV === "production" ? 80 : 3000;
+export const API_PORT = process.env.NODE_ENV === "production" ? "" : 3000;
 
 export const WS_ENDPOINT = process.env.NODE_ENV === "production" ? (
-  "http://cloud.chrisvilches.com/poller"
+  "https://cloud.chrisvilches.com/poller"
 ) : (
   "ws://localhost:3000"
 );
